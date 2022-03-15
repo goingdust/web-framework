2df16a656242d9a66e9f12c36a96c77503971ce1
goingdust <dustinluck@gmail.com>
2022-03-15 13:09:53 -0400

    Create on method that stores events and their given callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-user.set({ age: 45 });
+user.on('change', () => {});
+user.on('change', () => {});
+user.on('click', () => {});
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+console.log(user);
