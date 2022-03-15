2b06c142c1f580729cef9a4a22bef7a3faca4ad0
goingdust <dustinluck@gmail.com>
2022-03-15 12:46:52 -0400

    Write set method to update user props

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,6 @@
 interface UserProps {
-	name: string;
-	age: number;
+	name?: string;
+	age?: number;
 }
 
 export class User {
@@ -8,5 +8,9 @@ export class User {
 
 	get(propName: 'name' | 'age'): string | number {
 		return this.data[propName];
+	}
+
+	set(update: UserProps): void {
+		this.data = { ...this.data, ...update };
 	}
 }
