92bce7876adbc09c09501a2ef6a0f03b5e6bf2a1
goingdust <dustinluck@gmail.com>
2022-03-15 12:39:02 -0400

    Create User model with get method to get user props

---
+++ src/models/User.ts
@@ -0,0 +1,12 @@
+interface UserProps {
+	name: string;
+	age: number;
+}
+
+export class User {
+	constructor(private data: UserProps) {}
+
+	get(propName: 'name' | 'age'): string | number {
+		return this.data[propName];
+	}
+}
