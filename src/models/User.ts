a3bdc113ce66c51f3952f0ffbfbedffa2c8f51a7
goingdust <dustinluck@gmail.com>
2022-03-16 13:07:03 -0400

    Move get and set methods over to Attributes class and use advanced generic constraint

--- src/models/User.ts
+++ src/models/User.ts
@@ -11,14 +11,4 @@ export interface UserProps {
 export class User {
 	events: Eventing = new Eventing();
 	sync: Sync<UserProps> = new Sync<UserProps>(`${dbUrl}/users`);
-
-	constructor(private data: UserProps) {}
-
-	get(propName: 'id' | 'name' | 'age'): string | number {
-		return this.data[propName] || 'No data saved';
-	}
-
-	set(update: UserProps): void {
-		this.data = { ...this.data, ...update };
-	}
 }
