416dbeb2a494d6011f29536745ac608a9288e2b6
goingdust <dustinluck@gmail.com>
2022-03-16 13:50:36 -0400

    Recreate set method which updates attributes and triggers change event

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { User } from './models/User';
 const user = new User({ name: 'Rudder', age: 78 });
 
 user.on('change', () => {
-	console.log('change!');
+	console.log('User was changed!');
 });
 
-user.trigger('change');
+user.set({ name: 'newname' });
