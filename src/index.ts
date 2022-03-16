b671bf601a87808606b78dda8b75efacff847a96
goingdust <dustinluck@gmail.com>
2022-03-16 13:42:22 -0400

    Write get methods for on, trigger and get for User to access outside methods

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Rudder', age: 78 });
 
-user.events.on('change', () => {
+user.on('change', () => {
 	console.log('change!');
 });
 
-user.events.trigger('change');
+user.trigger('change');
