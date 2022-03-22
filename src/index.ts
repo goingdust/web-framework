0c45de3f807b635a61c9ae869fd6d5002f70482d
goingdust <dustinluck@gmail.com>
2022-03-22 17:00:53 -0400

    Create buildUserCollection method on User
    that creates instance of a user collection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
-import { User, UserProps } from './models/User';
-import { Collection } from './models/Collection';
+import { User } from './models/User';
 
-const collection = new Collection<User, UserProps>(
-	`${process.env.DB_URL}/users`,
-	(json: UserProps) => User.buildUser(json)
-);
+const collection = User.buildUserCollection();
 
 collection.on('change', () => {
 	console.log(collection);
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,6 +2,7 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { Eventing } from './Eventing';
 import { ApiSync } from './ApiSync';
+import { Collection } from './Collection';
 
 const dbUrl = process.env.DB_URL;
 
@@ -17,6 +18,13 @@ export class User extends Model<UserProps> {
 			new Attributes<UserProps>(attrs),
 			new Eventing(),
 			new ApiSync<UserProps>(`${dbUrl}/users`)
+		);
+	}
+
+	static buildUserCollection(): Collection<User, UserProps> {
+		return new Collection<User, UserProps>(
+			`${dbUrl}/users`,
+			(json: UserProps) => User.buildUser(json)
 		);
 	}
 }
