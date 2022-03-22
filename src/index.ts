c14194caa3842abe7f6874dab5f04193446bc10d
goingdust <dustinluck@gmail.com>
2022-03-22 16:54:32 -0400

    Refactor Collection to be more reusable with things other than User

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
+import { User, UserProps } from './models/User';
 import { Collection } from './models/Collection';
 
-const collection = new Collection(`${process.env.DB_URL}/users`);
+const collection = new Collection<User, UserProps>(
+	`${process.env.DB_URL}/users`,
+	(json: UserProps) => User.buildUser(json)
+);
 
 collection.on('change', () => {
 	console.log(collection);
